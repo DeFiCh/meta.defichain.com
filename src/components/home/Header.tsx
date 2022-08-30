10c9cc924fe3c03ee2718791f82d03d24ab12765
Keng <kengyeleow@gmail.com>
2022-08-30 17:30:09 +0800

    feature(ui-ux): add temp header

---
+++ src/components/home/Header.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@components/ui/Button";
+import Image from "next/image";
+import Link from "next/link";
+
+export function Header(): JSX.Element {
+  return (
+    <header>
+      <nav className="flex justify-between w-full ">
+        <Link className="flex items-center" href="/src/pages">
+          <div className="relative w-[149px]">
+            <Image
+              data-testid="logo"
+              layout="fill"
+              src="/logo.svg"
+              alt="DeFi Meta Chain Logo"
+              objectFit="contain"
+            />
+          </div>
+        </Link>
+        <Button
+          label={
+            <div className="flex items-center ">
+              <span className="pr-1.5 relative before:button-text before:content-['Get_DFI'] before:bg-white-50 before:opacity-100 group-hover:before:opacity-0 after:button-text after:content-['Get_DFI'] after:brand-gradient-1 after:opacity-0 group-hover:after:opacity-100">
+                Get DFI
+              </span>
+            </div>
+          }
+          onClick={() => {/**TODO: add handler**/ }}
+        />
+      </nav>
+    </header>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
+import { Header } from "@components/home/Header";
 import clsx from "clsx";
 
 function Home() {
   return (
-    <section
-      className={clsx("bg-white-100 relative min-h-screen flex flex-col")}
-    >
-      <h1 data-testid="title_keng_ye_delete_me">DeFi Meta Chain</h1>
-    </section>
+    <Header/>
   );
 }
 
