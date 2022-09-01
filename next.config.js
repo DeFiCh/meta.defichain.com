d6a7d42198e01b94ac3a4b57d3923d29fa0328f3
JJ Adonis <work@josejai.me>
2022-09-01 17:15:08 +0800

    chore(deps): prettier

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
   swcMinify: true,
+  reactStrictMode: true,
   images: {
     deviceSizes: [360, 768, 1440, 1920, 2048, 3840],
   },
