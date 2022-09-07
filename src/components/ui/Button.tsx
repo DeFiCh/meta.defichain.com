7899f6ae887a23625f543b7f2db78f079c240819
Keng <kengyeleow@gmail.com>
2022-09-07 16:44:57 +0800

    feat: add content for 1st and 2nd sections
    Including bg and jellyfish for mobile view

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,9 +1,11 @@
 export default function Button({
   label,
+  href,
   onClick,
   customStyle,
 }: {
-  label: string | React.ReactElement;
+  label: string;
+  href: string;
   onClick?: () => void;
   customStyle?: string;
 }): JSX.Element {
@@ -15,7 +17,16 @@ export default function Button({
       }`}
       onClick={onClick}
     >
-      {label}
+      <a href={href} target="_blank" rel="noreferrer">
+        <div className="flex items-center ">
+          <span
+            data-label={label}
+            className="relative before:button-text before:content-[attr(data-label)] before:bg-white-50 before:opacity-100 group-hover:before:opacity-0 after:button-text after:content-[attr(data-label)] after:brand-gradient-1 after:opacity-0 group-hover:after:opacity-100"
+          >
+            {label}
+          </span>
+        </div>
+      </a>
     </button>
   );
 }
