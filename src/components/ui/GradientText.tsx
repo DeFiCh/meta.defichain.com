819091ba2999da4efb3ab8bf15968ad03ede06cf
Keng <kengyeleow@gmail.com>
2022-09-09 14:19:31 +0800

    feat: create reusable component for gradient text

---
+++ src/components/ui/GradientText.tsx
@@ -0,0 +1,23 @@
+export default function GradientText({
+  label,
+  initialTextColor = "bg-white-50",
+  customStyle,
+  gradient = "after:brand-gradient-1",
+}: {
+  label: string;
+  initialTextColor?: string;
+  customStyle?: string;
+  gradient?: string;
+}): JSX.Element {
+  return (
+    <span
+      data-label={label}
+      data-gradient={gradient}
+      className={`relative before:button-text before:content-[attr(data-label)] before:${initialTextColor} before:opacity-100 group-hover:before:opacity-0 after:button-text after:content-[attr(data-label)] ${gradient} after:opacity-0 group-hover:after:opacity-100 ${
+        customStyle ?? ""
+      }`}
+    >
+      {label}
+    </span>
+  );
+}
