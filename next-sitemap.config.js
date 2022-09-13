f0e1414aae6cff4edff8598676493268233afbb9
thedoublejay <work@josejai.me>
2022-09-13 13:58:06 +0800

    chore(setup): sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://defimetachain.org',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{
+      userAgent: '*',
+      allow: '/',
+    }]
+  }
+}
