0b87e6b40650c7283aed448588372b05aaf7eb63
Nalin Dwivedi <dwivedi.nalin2@gmail.com>
2022-11-15 11:39:19 +0530

    Updated eslintrc for underfined jsx warning and fixed linting error

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -8,7 +8,7 @@ export default function Header(): JSX.Element {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   return (
     <header>
-      <nav className="container mx-auto flex justify-between w-full items-center" data-testid={"mob_header"}>
+      <nav className="container mx-auto flex justify-between w-full items-center" data-testid="mob_header">
         <Link className="flex items-center" href="/">
           <div className="relative w-[203px] h-[36px]">
             <Image
@@ -34,7 +34,7 @@ export default function Header(): JSX.Element {
           <svg
             className="feather w-6 h-6 stroke-white-50 block min-[1361px]:hidden md:ml-6"
             onClick={() => setIsMobileMenuOpen(true)}
-            data-testid={"hamburger_icon"}
+            data-testid="hamburger_icon"
           >
             <use href="/feather-sprite.svg#menu" />
           </svg>
