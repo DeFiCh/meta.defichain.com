1d69b272f59805cc866fefb8d8001eb243fdb47a
Mikhail <zig-92@ya.ru>
2022-11-22 12:26:05 +0800

    removed unused command;

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,6 +1,5 @@
 declare namespace Cypress {
   interface Chainable {
-    verifyHeaderFooter(locator: string): void;
     verifyElementContains(locator: string, label: string): void;
     verifyLinks(locator: string, content: string, link: string): void;
   }
