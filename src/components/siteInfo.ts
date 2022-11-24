a88605ed36d6ddebdda8cde513deeff9c32afad2
Keng Ye <kengyeleow@gmail.com>
2022-11-24 16:10:47 +0800

    chore: update short desc and site title

--- src/components/siteInfo.ts
+++ src/components/siteInfo.ts
@@ -1,8 +1,8 @@
 export const appName = "DeFi Meta Chain";
-export const shortDescription = "Stretch the definition of Native DeFi.";
+export const shortDescription = "Unlock the Limitless Possibilities of DeFiChain";
 export const longDescription =
   "Build on DeFi Meta Chain, a powerful EVM-compatible Layer 2 blockchain that combines the Security of Bitcoin with the Scalability of Ethereum.";
-export const siteTitle = `DeFi Meta Chain | Stretch the definition of Native DeFi`;
+export const siteTitle = `DeFi Meta Chain | Unlock the Limitless Possibilities of DeFiChain`;
 export const website = "https://defimetachain.org/";
 export const logo = "/ico/android-chrome-512x512.png";
 export const brWebsite = "https://birthday.dev/";
