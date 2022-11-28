35ff3e771b0f6f255e2cda676e73a7620cc5c13a
JJ Adonis <work@josejai.me>
2022-11-28 15:56:29 +0800

    chore(deps): update description

--- src/components/siteInfo.ts
+++ src/components/siteInfo.ts
@@ -2,7 +2,7 @@ export const appName = "DeFi Meta Chain";
 export const shortDescription =
   "Unlock the Limitless Possibilities of DeFiChain";
 export const longDescription =
-  "Build on DeFi Meta Chain, a powerful EVM-compatible Layer 2 blockchain that combines the Security of Bitcoin with the Scalability of Ethereum.";
+  "Build on DeFi Meta Chain, a powerful EVM-compatible Layer 2 blockchain that combines the Security of Bitcoin with the Flexibility of Ethereum.";
 export const siteTitle = `DeFi Meta Chain | Unlock the Limitless Possibilities of DeFiChain`;
 export const website = "https://meta.defichain.com/";
 export const logo = "/ico/android-chrome-512x512.png";
